428fc922a59fe07489e93100d736f683ff059361
Shubham Garg <91garg.shubham@gmail.com>
2022-12-15 09:56:47 +0530

    error handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:singularity_flutter/SingularityFlutter.dart';
 
 void main() {
@@ -15,31 +17,98 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> implements SigularitySDKProtocol {
   /// take variable of SingularityFlutter
   late SingularityFlutter _gamepay;
+  late TextEditingController _controller;
+
+  // _MyAppState(){
+  //   _controller = TextEditingController();
+  //   _controller.text = "0";
+  // }
 
   @override
   void initState() {
     super.initState();
+
+    _controller = TextEditingController();
+    _controller.text = "0";
 
     /// initalize variable of SingularityFlutter
     _gamepay = SingularityFlutter(this);
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Niobrix'),
+          title: const Text('Neobrix'),
         ),
-        body: Center(
-          child: ElevatedButton(
-            child: const Text("Start"),
-            onPressed: () async {
-              /// call SingularityFlutter openLoginScreen to start login flow or open profile page
-              _gamepay.openLoginScreen(key: "neobrix", singularityConfig: {});
-            },
-          ),
-        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+             Row(
+               mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text("0 -> Sandbox, 1-> Production"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text("Choose Environment: ", style: TextStyle(fontSize: 16),),
+              ],
+            ),
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ),
+            ElevatedButton(onPressed: () async {
+              // call SingularityFlutter openLoginScreen to start login flow or open profile page
+              var environment =  _controller.text;
+
+              try{
+                var env = int.parse(environment);
+                if(env!= 0 && env!= 1){
+                  Fluttertoast.showToast(
+                      msg: "Invalid Input",
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.CENTER,
+                      timeInSecForIosWeb: 1,
+                      textColor: Colors.white,
+                      fontSize: 16.0
+                  );
+                  return;
+                }
+
+                _gamepay.openLoginScreen(
+                    key: "neobrix", singularityConfig: {'environment': env}
+                );
+              }
+              catch(e){
+                Fluttertoast.showToast(
+                    msg: "Invalid Input",
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.CENTER,
+                    timeInSecForIosWeb: 1,
+                    textColor: Colors.white,
+                    fontSize: 16.0
+                );
+              }
+
+
+
+            }, child: Text("Launch SDK"))
+
+          ],
+        )
       ),
     );
   }
@@ -59,7 +128,7 @@ class _MyAppState extends State<MyApp> implements SigularitySDKProtocol {
             title: Text(title),
             content: SingleChildScrollView(
               scrollDirection: Axis.vertical,
-              child: new Text(content),
+              child: Text(content),
             ),
             actions: [okButton],
           );
@@ -68,7 +137,7 @@ class _MyAppState extends State<MyApp> implements SigularitySDKProtocol {
 
   /// Sigularity delegate method after login success
   @override
-  onGetSingularityUserInfo(String user) {
+  onGetSingularityUserInfo(Map user) {
     showAlertDialog(context, "onGetSingularityUserInfo", user.toString());
   }
 
@@ -81,6 +150,12 @@ class _MyAppState extends State<MyApp> implements SigularitySDKProtocol {
   /// Sigularity delegate method after logout success
   @override
   onSingularityLogout() {
-    showAlertDialog(context, "onSigularityLogout", "onSigularityLogout");
+    showAlertDialog(context, "onSigularityLogout", "logout");
+  }
+  
+  /// Sigularity delegate method when any error occured
+  @override
+   onSingularityError(String message) {
+    showAlertDialog(context, "onSigularityLogout", message);
   }
 }
